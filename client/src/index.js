bfe73fb62d5bff37b98c52f5b62ade0dd05ea4ef
duongductrung1708 <trungyna1708@gmail.com>
2024-06-24 13:16:04 +0700

    need to fix get user

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import reportWebVitals from "./reportWebVitals.js";
-import * as serviceWorker from "./serviceWorker";
 import { AuthProvider } from "./hooks/useAuth";
 import App from "./App";
 
